8820c2874f42a31925c130b44f918947fd320528
Roni <ronniiii@yahoo.com>
2023-03-06 18:23:14 +0100

    [RESTRUCTURE]Made portfolio card separate

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const ProjectCard = ({ project, handleSelectTag }) => {
+  return (
+    <div className="project-card" key={project.id}>
+      <div className="img">
+        <img src={project.image} alt={project.title} />
+        <div className="button-group flex justify-evenly align-center">
+          <a
+            href={project.demoUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View Demo
+          </a>
+          <a
+            href={project.sourceUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View Source
+          </a>
+        </div>
+      </div>
+      <div className="project-details">
+        <h3>{project.title}</h3>
+        <p>{project.description}</p>
+        <div className="technologies flex align-center">
+          {project.technologies.map((technology) => (
+            <a
+              href="javacript:void(0);"
+              key={technology}
+              onClick={() => handleSelectTag(technology)}
+            >
+              {technology}
+            </a>
+          ))}
+        </div>
+        <div className="button-group">
+          <a
+            href={project.demoUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View Demo
+          </a>
+          <a
+            href={project.sourceUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View Source
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import projectsData from "../data/data";
+import Card from "../components/ProjectCard";
 
 function Portfolio() {
   const [selectedTag, setSelectedTag] = useState(null);
@@ -19,57 +20,11 @@ function Portfolio() {
       <h2>My Projects</h2>
       <div className="project-list flex justify-center align-center wrap">
         {filteredProjects.map((project) => (
-          <div className="project-card" key={project.id}>
-            <div className="img">
-              <img src={project.image} alt={project.title} />
-              <div className="button-group flex justify-evenly align-center">
-                <a
-                  href={project.demoUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Demo
-                </a>
-                <a
-                  href={project.sourceUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Source
-                </a>
-              </div>
-            </div>
-            <div className="project-details">
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-              <div className="technologies flex align-center">
-                {project.technologies.map((technology) => (
-                  <a href="javacript:void(0);"
-                    key={technology}
-                    onClick={() => handleSelectTag(technology)}
-                  >
-                    {technology}
-                  </a>
-                ))}
-              </div>
-              <div className="button-group">
-                <a
-                  href={project.demoUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Demo
-                </a>
-                <a
-                  href={project.sourceUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Source
-                </a>
-              </div>
-            </div>
-          </div>
+          <Card
+            project={project}
+            handleSelectTag={handleSelectTag}
+            key={project.id}
+          />
         ))}
       </div>
     </section>
